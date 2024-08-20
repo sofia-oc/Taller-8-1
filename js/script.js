2d867bd32c484b3662877aca57c2d00dce7030d9
Sofia Osano Corbo <sofia-199906@hotmail.com>
2024-08-19 23:02:48 -0300

    funcion fetch

--- js/script.js
+++ js/script.js
@@ -19,3 +19,11 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+fetch(DATA_URL)
+  .then(response => response.json())
+  .then(names => {
+    showData(names.students);
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  })
